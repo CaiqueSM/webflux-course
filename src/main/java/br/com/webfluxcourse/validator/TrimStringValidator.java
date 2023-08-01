883365c8ebb98a8988b4384a6e1bf97217bbdaa3
caimachado_meli <caique.machado@mercadolivre.com>
2023-08-01 19:40:16 -0300

    add string validator

---
+++ src/main/java/br/com/webfluxcourse/validator/TrimStringValidator.java
@@ -0,0 +1,15 @@
+package br.com.webfluxcourse.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class TrimStringValidator implements ConstraintValidator<TrimString, String> {
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        if(value == null) {
+            return true;
+        }
+
+        return value.trim().length() == value.length();
+    }
+}
